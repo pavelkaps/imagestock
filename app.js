5533a258d8a7847596700f0c157d8302b874be06
pavelkaps <kaps.pavel@gmail.com>
2017-03-01 16:18:41 +0400

    Fix flex gallery(with freewall)

--- app.js
+++ app.js
@@ -2,5 +2,38 @@
  * Created by Паша on 24.02.2017.
  */
 (function () {
-    angular.module('ImageGallery', ['ngRoute','angularCSS', 'ngMaterial'])
+    angular.module('ImageGallery', ['ngRoute','angularCSS', 'ngMaterial']).directive('repeatBrick', function() {
+        return function(scope, element, attrs) {
+            if (scope.$last){
+                console.log("emmit");
+                scope.$emit('LastBrick');
+            }
+        }
+    })
+        .directive('theFreewall', function($window) {
+            return function(scope, element, attrs) {
+                scope.$on('LastBrick', function(event){
+                    var wall = new Freewall("#freewall");
+                    console.log("directive");
+                    wall.reset({
+                        selector: '.card',
+                        draggable: true,
+                        animate: true,
+                        cellW: 238,
+                        cellH: 190,
+                        onResize: function() {
+                            wall.fitHeight($window.innerHeight);
+                        }
+                        
+                    });
+
+                    wall.container.find('.card').on('load', (function() {
+                        wall.fitHeight($window.innerHeight);
+                    }));
+
+                    $window.dispatchEvent(new Event('resize'));
+
+                });
+            };
+        });
 })();
